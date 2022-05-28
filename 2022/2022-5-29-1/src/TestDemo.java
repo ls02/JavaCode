89e2c983c49c395c64e744c4fe908288e59db554
ls02 <2877455773@qq.com>
2022-05-29 01:24:23 +0800

    practice code

---
+++ 2022/2022-5-29-1/src/TestDemo.java
@@ -0,0 +1,29 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86176
+ * Date: 2022-05-29
+ * Time: 0:13
+ */
+public class TestDemo {
+    static void Test1() {
+        int count = 0;
+        for (int i = 1; i < 100; i++) {
+            if (i % 9 == 0)
+            {
+                if ((i / 10) / 9 == 0)
+                {
+                    count++;
+                }
+
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        Test1();
+    }
+}
