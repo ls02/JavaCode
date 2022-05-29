ffb9e91faa91a89ab94a5f701b3185657c84e62e
ls02 <2877455773@qq.com>
2022-05-29 17:41:35 +0800

    practice code

--- 2022/2022-5-29-2/src/TestDemo.java
+++ 2022/2022-5-29-2/src/TestDemo.java
@@ -87,8 +87,23 @@ public class TestDemo {
         sc.close();
     }
 
+    static void Test4() {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.printf("%d*%d=%-2d\t", i, j, i * j);
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+
     public static void main(String[] args) {
-        Test3();
+        Test4();
+//        Test3();
 //        Test2();
 //        Test1();
     }
