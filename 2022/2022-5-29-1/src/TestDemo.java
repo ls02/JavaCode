d0924d028235ad54b7c6ff91d58a5db14b413861
ls02 <2877455773@qq.com>
2022-05-29 14:26:45 +0800

    practice code

--- 2022/2022-5-29-1/src/TestDemo.java
+++ 2022/2022-5-29-1/src/TestDemo.java
@@ -129,7 +129,7 @@ public class TestDemo {
 
     static void Test7() {
         for (int i = 100; i <= 999; i++) {
-            int num1 = i / 10 / 10  % 10;
+            int num1 = i / 10 / 10 % 10;
             int num2 = i / 10 % 10;
             int num3 = i % 10;
 
@@ -140,9 +140,22 @@ public class TestDemo {
         }
     }
 
+        static void Test8() {
+            int flag = 1;
+            double sum = 0.0;
+
+            for (int i = 1; i < 100; i++) {
+                sum += 1.0 * (double)flag / (double)i;
+                flag *= -1;
+            }
+
+            System.out.println(sum);
+        }
+
     public static void main(String[] args) {
 
-        Test7();
+        Test8();
+//        Test7();
 //        Test6();
 //        Test5();
 //        Test4();
