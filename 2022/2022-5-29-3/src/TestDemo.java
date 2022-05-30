f8ea7ecce2a27c4912b638ffaec563888d419d16
ls02 <2877455773@qq.com>
2022-05-30 18:52:18 +0800

    practice code

---
+++ 2022/2022-5-29-3/src/TestDemo.java
@@ -0,0 +1,23 @@
+import javax.naming.PartialResultException;
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86176
+ * Date: 2022-05-29
+ * Time: 22:49
+ * @author 86176
+ */
+public class TestDemo {
+    static void test1() {
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        int a = sc.nextInt();
+
+        sc.close();
+    }
+}
---
+++ 2022/5-30-1/src/TestDemo.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86176
+ * Date: 2022-05-30
+ * Time: 10:18
+ * @author 86176
+ */
+
+public class TestDemo {
+
+    static void Test1() {
+        int[] arr = { 1, 2, 3, 4 };
+        int[] copy = arr.clone();
+        System.out.println(Arrays.toString(copy));
+//        int[] arr = { 1, 2, 3, 4 };
+//        int[] copy = new int[arr.length];
+//        System.arraycopy(arr, 0, copy, 0, arr.length);
+//        System.out.println(Arrays.toString(copy));
+//        int[] arr = { 1, 2, 3, 4 };
+//        int[] ret = Arrays.copyOf(arr, arr.length * 2);
+//        System.out.println(Arrays.toString(ret));
+    }
+
+    static int Max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    static double Max(double a, double b) {
+        return a > b ? a : b;
+    }
+
+    static void Test2() {
+        System.out.println(Max(1, 3));
+        System.out.println(Max(2.3, 4.5));
+    }
+
+    public static void main(String[] args) {
+        Test2();
+//        Test1();
+    }
+}
