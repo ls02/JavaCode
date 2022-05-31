54befc01f8e8fdaeca2ea9b8aecfbe0b29c295b2
ls02 <2877455773@qq.com>
2022-05-31 15:31:29 +0800

    practice code

--- 2022/5-31-1/src/TestDemo.java
+++ 2022/5-31-1/src/TestDemo.java
@@ -35,7 +35,33 @@ public class TestDemo {
         System.out.println(Arrays.toString(arr));
     }
 
+    static void SwapArray(int[] arr1, int[] arr2) {
+        int len = arr1.length;
+
+        if (len > arr2.length) {
+            len = arr2.length;
+        }
+
+        for (int i = 0; i < len; i++) {
+            int temp = arr1[i];
+            arr1[i] = arr2[i];
+            arr2[i] = temp;
+        }
+
+    }
+
+    static void Test2() {
+        int[] arr1 = { 1, 2, 3, 4 };
+        int[] arr2 = { 4, 3, 2, 1 };
+
+        SwapArray(arr1, arr2);
+
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+    }
+
     public static void main(String[] args) {
-        Test1();
+        Test2();
+//        Test1();
     }
 }
