fbc34d3e595027d8d86b9732c9e704d288065c9b
ls02 <2877455773@qq.com>
2022-05-31 18:41:24 +0800

    Added insert and delete interfaces

--- 2022/5-31-SeqList/src/SeqList.java
+++ 2022/5-31-SeqList/src/SeqList.java
@@ -24,21 +24,24 @@ public class SeqList {
      * 默认构造
      */
     public SeqList() {
-        _data = new int[10];
+        _data = new int[4];
         _size = 0;
-        _capacity = 10;
+        _capacity = 4;
     }
 
     /**
      * 拷贝构造
-     * @param sqeL 传递一个对象
+     * @param seqL 传递一个对象
      */
-    public SeqList(SeqList sqeL) {
-        _data = new int[sqeL._size];
+    public SeqList(SeqList seqL) {
+        _data = new int[seqL._size];
 
-        for (int i = 0; i < sqeL._size; i++) {
+        for (int i = 0; i < seqL._size; i++) {
             _data[i] = seqL._data[i];
         }
+
+        _size = seqL._size;
+        _capacity = seqL._capacity;
     }
 
     /**
@@ -49,5 +52,103 @@ public class SeqList {
         return _size;
     }
 
+    /**
+     * 尾插数据
+     * @param val 插入的值
+     */
+    public void PushBack(int val) {
+        CheckCapacity();
+
+        _data[_size] = val;
+        _size++;
+    }
+
+    /**
+     * pos位置插入
+     * @param pos 要插入的位置
+     * @param val 插入的数据
+     */
+    public void Insert(int pos, int val) {
+        if (pos < 0 || pos > _capacity) {
+            System.out.println("插入位置非法！");
+
+            return;
+        }
+
+        CheckCapacity();
+
+        for (int i = _size; i >= pos; i--) {
+            _data[i + 1] = _data[i];
+        }
+
+        _data[pos] = val;
+        _size++;
+    }
+
+    /**
+     * 尾删一个数据
+     */
+    public void PopBack() {
+        if (_size == 0) {
+            return;
+        }
+
+        _size--;
+    }
+
+    public void Erase(int pos) {
+        if (pos < 0 || pos > _capacity) {
+            System.out.println("删除位置非法");
+
+            return;
+        }
+
+        if (_size == 0) {
+            return;
+        }
+
+        for (int i = pos + 1; i < _size; i++) {
+            _data[i - 1] = _data[i];
+        }
+
+        _size--;
+    }
+
+    /**
+     * 检查是否需要扩容
+     */
+    private void CheckCapacity() {
+        if (_size == _capacity) {
+            int[] newData = new int[_capacity * 2];
+
+            for (int i = 0; i < _size; i++) {
+                newData[i] = _data[i];
+            }
+
+            _data = newData;
+            _capacity *= 2;
+        }
+    }
+
+    /**
+     * 拷贝数据
+     * @param copy 要拷贝的对象
+     */
+    public void CopyOfArr(int[] copy) {
+        _data = new int[copy.length];
+        for (int i = 0; i < _size; i++) {
+            _data[i] = copy[i];
+        }
+
+        _size = copy.length;
+    }
+
+    public void Display() {
+        for (int i = 0; i < _size; i++) {
+            System.out.print(_data[i] + " ");
+        }
+
+        System.out.println();
+    }
 
 }
--- 2022/5-31-SeqList/src/TestDemo.java
+++ 2022/5-31-SeqList/src/TestDemo.java
@@ -6,4 +6,50 @@
  * Time: 17:38
  */
 public class TestDemo {
+    public static void main(String[] args) {
+        SeqList s1 = new SeqList();
+
+        s1.PushBack(1);
+        s1.PushBack(2);
+        s1.PushBack(3);
+        s1.PushBack(4);
+        s1.PushBack(5);
+        s1.PushBack(6);
+        s1.PushBack(7);
+
+        SeqList s2 = s1;
+
+        s1.Display();
+        s2.Display();
+
+        System.out.println(s1.Size());
+
+        s1.Erase(0);
+        s1.Erase(0);
+        s1.Erase(0);
+        s1.Erase(0);
+        s1.Erase(0);
+        s1.Erase(0);
+        s1.Erase(0);
+        s1.Erase(0);
+        s1.Erase(0);
+
+        s1.Insert(0, 1);
+        s1.Insert(0, 2);
+        s1.Insert(0, 3);
+        s1.Insert(0, 4);
+        s1.Insert(0, 5);
+
+        s1.PopBack();
+
+        s1.Display();
+        System.out.println(s1.Size());
+
+        s1.PopBack();
+        s1.PopBack();
+        s1.PopBack();
+        s1.PopBack();
+        s1.PopBack();
+        System.out.println(s1.Size());
+    }
 }
