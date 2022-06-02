8e8c86ccedf53b8d83fc8a08a9b40f8b54169d7b
ls02 <2877455773@qq.com>
2022-06-02 21:32:15 +0800

    practice code

---
+++ 2022/6-2-1/src/TestDemo.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86176
+ * Date: 2022-06-02
+ * Time: 16:16
+ */
+public class TestDemo {
+}
---
+++ 2022/6-2-1/src/com/ls/demo/TestDemo.java
@@ -0,0 +1,11 @@
+package com.ls.demo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86176
+ * Date: 2022-06-02
+ * Time: 16:24
+ */
+public class TestDemo {
+}
