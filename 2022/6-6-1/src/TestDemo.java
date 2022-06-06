da17565aa562bb788b8b32c88d2edf959d0587c6
ls02 <2877455773@qq.com>
2022-06-06 22:04:05 +0800

    Practice code

---
+++ 2022/6-6-1/src/TestDemo.java
@@ -0,0 +1,17 @@
+import java.util.Vector;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86176
+ * Date: 2022-06-06
+ * Time: 22:01
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        Vector<Integer> v1 = new Vector<>();
+        v1.add(3);
+
+        System.out.println(v1);
+    }
+}
