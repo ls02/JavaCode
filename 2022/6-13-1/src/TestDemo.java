7ba023e4b91483ad68d259de50c75fca5d870beb
ls02 <2877455773@qq.com>
2022-06-14 16:57:27 +0800

    practice code

--- 2022/6-13-1/src/TestDemo.java
+++ 2022/6-13-1/src/TestDemo.java
@@ -25,7 +25,7 @@ class MyThread2  extends Thread {
 }
 
 public class TestDemo {
-    public static void main(String[] args) throws InterruptedException {
+    static void Test1() {
         Thread t = new MyThread2();
         t.start();
 
@@ -37,5 +37,34 @@ public class TestDemo {
                 e.printStackTrace();
             }
         }
+    }
+
+    static void Test2(){
+        Thread t = new Thread(new MyThread2());
+        t.start();
+    }
+
+    static void Test3() {
+        //这个语法就是匿名内部类
+        //相当于创建了一个匿名的类，这个类继承了Thread
+        //此处new的实列，其实就是new了这个新的类的实列
+        Thread t = new Thread() {
+            public void run() {
+                System.out.println("hello thread");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        t.start();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+//        Test3();
+//        Test2();
+        Test1();
     }
 }
