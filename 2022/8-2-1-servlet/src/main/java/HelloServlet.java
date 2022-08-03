1c5290bbfbda2421b243eaadd3067b6ac0b51a7f
ls02 <2877455773@qq.com>
2022-08-03 17:46:03 +0800

    practice code

---
+++ 2022/8-2-1-servlet/src/main/java/HelloServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86176
+ * Date: 2022-08-03
+ * Time: 13:48
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+//        System.out.println("hello");
+        resp.getWriter().write("hello");
+    }
+}
---
+++ 2022/8-2-1-servlet/src/main/java/MethodServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86176
+ * Date: 2022-08-03
+ * Time: 17:31
+ */
+
+@WebServlet("/method")
+public class MethodServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain; charset=utf-8");
+        resp.getWriter().write("Get 响应");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("POST response");
+    }
+}
