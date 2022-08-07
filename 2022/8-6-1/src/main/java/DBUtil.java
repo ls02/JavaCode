de0b47da47cb0280ea058d052ffc532e431f4594
ls02 <2877455773@qq.com>
2022-08-07 22:37:41 +0800

    practice code

---
+++ 2022/8-6-1/src/main/java/DBUtil.java
@@ -0,0 +1,33 @@
+import com.mysql.cj.jdbc.MysqlDataSource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86176
+ * Date: 2022-08-07
+ * Time: 15:41
+ */
+public class DBUtil {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/1?useSSL=false&characterEncoding=utf8";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "2222";
+    private static DataSource dataSource = null;
+
+    public static DataSource getDataSource() {
+        if (dataSource == null) {
+            dataSource = new MysqlDataSource();
+            ((MysqlDataSource)dataSource).setURL(URL);
+            ((MysqlDataSource)dataSource).setUser(USERNAME);
+            ((MysqlDataSource)dataSource).setPassword(PASSWORD);
+        }
+
+        return dataSource;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
+}
--- 2022/8-6-1/src/main/java/MessageServlet.java
+++ 2022/8-6-1/src/main/java/MessageServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,37 +26,91 @@ class Message {
 @WebServlet("/message")
 public class MessageServlet extends HttpServlet {
     private ObjectMapper objectMapper = new ObjectMapper();
-    private List<Message> messageList = new ArrayList<>();
+//    private List<Message> messageList = new ArrayList<>();
 
-    public void init() throws ServletException {
+//    这个是保存文件的路径
+//    private String filePath = "d:/message.txt";
+
+//    public void init() throws ServletException {
+
 //        在这里面给 messageList 构造几个测试数据，方便来观察，
 //        这个数据的构造可以写到 init 里，也可以写道构造方法里
-        Message message = new Message();
-        message.from = "黑猫";
-        message.to = "白猫";
-        message.message = "喵";
-        messageList.add(message);
-
-        message = new Message();
-        message.from = "橘猫";
-        message.to = "黑猫";
-        message.message =  "喵呜";
-        messageList.add(message);
-    }
+//        Message message = new Message();
+//        message.from = "黑猫";
+//        message.to = "白猫";
+//        message.message = "喵";
+//        messageList.add(message);
+//
+//        message = new Message();
+//        message.from = "橘猫";
+//        message.to = "黑猫";
+//        message.message =  "喵呜";
+//        messageList.add(message);
+//    }
 
 //    这个方法用来处理，从服务器获取的消息数据
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json;charset=utf-8");
-        objectMapper.writeValue(resp.getWriter(), messageList);
+//        objectMapper.writeValue(resp.getWriter(), messageList);
     }
 
 //    这个方法用来处理，从客户端提交数据给服务器
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Message message = objectMapper.readValue(req.getInputStream(), Message.class);
-        messageList.add(message);
+        List<Message> messageList = load();
+        objectMapper.writeValue(resp.getWriter(), messageList);
+//        messageList.add(message);
+
+//        在这里，要进行一个 “写文件” 操作
+        save(message);
+
         resp.setContentType("application/json;charset=utf-8");
         resp.getWriter().write("{\"OK\": 1}");
+    }
+
+    private List<Message> load() {
+//        这个方法负责读取文件，把读到的数据获取到之后，放到 List<Message> 中
+        List<Message> messageList = new ArrayList<>();
+//        System.out.println("从文件中加载！");
+//
+////        此处我们需要按行读取， FileReader 本身不支持，需要套上一层 BufferedReader
+//        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+//            while (true) {
+//                String line = bufferedReader.readLine();
+//                if (null == line) {
+//                    break;
+//                }
+//
+////                如果读取到了 line 的内容就把 line 解析成一个 Message 对象
+//                String[] tokens = line.split("\t");
+//                Message message  = new Message();
+//                message.from = tokens[0];
+//                message.to = tokens[1];
+//                message.message = tokens[2];
+//                messageList.add(message);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+        System.out.println("从数据库读取数据！");
+
+        return messageList;
+
+    }
+
+    private void save(Message message) {
+//        System.out.println("向文件中写入数据！");
+//        try (FileWriter fileWriter = new FileWriter(filePath)) {
+////            写入文件的格式有很多方式，可以直接写 json ，也可以使用行文本（每个记录占一行，字段之间使用分隔符区分）
+//            fileWriter.write(message.from + "\t" + message.to + "\t" + message.message + "\n");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        System.out.println("向数据库中写入数据！");
+//        1.先和数据库建立连接
     }
 }
