225b67f76b4d0d18fcdfa85e37b7488640f6c355
ls02 <2877455773@qq.com>
2022-08-09 16:12:40 +0800

    practice code

--- 2022/8-6-1/src/main/java/DBUtil.java
+++ 2022/8-6-1/src/main/java/DBUtil.java
@@ -1,6 +1,8 @@
 import com.mysql.cj.jdbc.MysqlDataSource;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -14,14 +16,18 @@ public class DBUtil {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/1?useSSL=false&characterEncoding=utf8";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "2222";
-    private static DataSource dataSource = null;
+    private static volatile DataSource dataSource = null;
 
     public static DataSource getDataSource() {
         if (dataSource == null) {
-            dataSource = new MysqlDataSource();
-            ((MysqlDataSource)dataSource).setURL(URL);
-            ((MysqlDataSource)dataSource).setUser(USERNAME);
-            ((MysqlDataSource)dataSource).setPassword(PASSWORD);
+            synchronized (DBUtil.class) {
+                if (dataSource == null) {
+                    dataSource = new MysqlDataSource();
+                    ((MysqlDataSource)dataSource).setURL(URL);
+                    ((MysqlDataSource)dataSource).setUser(USERNAME);
+                    ((MysqlDataSource)dataSource).setPassword(PASSWORD);
+                }
+            }
         }
 
         return dataSource;
@@ -29,6 +35,31 @@ public class DBUtil {
 
     public static Connection getConnection() throws SQLException {
         return getDataSource().getConnection();
-//        return getDataSource().getConnection();
+    }
+
+    public static void close(Connection connection, PreparedStatement statement, ResultSet resultSet){
+        if (null != resultSet) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (null != statement) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (null != connection) {
+            try{
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- 2022/8-6-1/src/main/java/MessageServlet.java
+++ 2022/8-6-1/src/main/java/MessageServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -112,5 +115,29 @@ public class MessageServlet extends HttpServlet {
 
         System.out.println("向数据库中写入数据！");
 //        1.先和数据库建立连接
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+//            1.显赫数据库建立连接
+            connection = DBUtil.getConnection();
+//            2.构造拼装 SQL
+            String sql =  "insert into message value s (?, ?, ?)";
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, message.from);
+            statement.setString(2, message.to);
+            statement.setString(3, message.message);
+
+//            3.执行 SQL
+            int ret = statement.executeUpdate();
+            if (ret == 1) {
+                System.out.println("插入成功！");
+            } else {
+                System.out.println("插入失败！");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection, statement, null);
+        }
     }
 }
