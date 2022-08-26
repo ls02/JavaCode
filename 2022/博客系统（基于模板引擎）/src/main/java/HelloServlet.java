9ad047c34d2dbffb91d75bed1fd89259191d4ab3
ls02 <2877455773@qq.com>
2022-08-26 17:19:33 +0800

    practice code

---
+++ 2022/博客系统（基于模板引擎）/src/main/java/HelloServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86176
+ * Date: 2022-08-25
+ * Time: 21:47
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("hello");
+    }
+}
