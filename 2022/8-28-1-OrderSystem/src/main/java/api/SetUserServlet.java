c9f78d361b26ff9e7b96372ce3a9efab17b73928
ls02 <2877455773@qq.com>
2022-09-06 13:00:40 +0800

    practice code

--- 2022/8-28-1-OrderSystem/src/main/java/api/SetUserServlet.java
+++ 2022/8-28-1-OrderSystem/src/main/java/api/SetUserServlet.java
@@ -71,4 +71,90 @@ public class SetUserServlet extends HttpServlet {
             resp.getWriter().write(jsonString);
         }
     }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("application/json; charset=utf-8");
+        OrderServlet.Response response = new OrderServlet.Response();
+
+        try {
+            //      1. 读取 body 中的数据
+            String body = OrderSystemUtil.readBody(req);
+//      2. 把 body 数据解析成 Request 对象（GSON）
+            RegisterServlet.Request request = gson.fromJson(body, RegisterServlet.Request.class);
+
+            UserDao userDao = new UserDao();
+            String name = req.getParameter("name");
+            String numb = request.numb;
+            String address = request.address;
+            int flags = Integer.parseInt(request.flags);
+            String password = request.password;
+//            flags 为 1 是修改密码
+//            flags 为 2 是修改地址
+//            flags 为 3 是修改手机号
+            if (flags == 1) {
+                userDao.setPassword(name, password);
+            } else if (flags == 2) {
+                userDao.setAddress(name, address);
+            } else if (flags == 3) {
+                userDao.setNumb(name, numb);
+            } else {
+                throw new OrderSystemException("未定义的标识符");
+            }
+
+            response.ok = 1;
+            response.reason = "";
+        } catch (OrderSystemException e) {
+            response.ok = 0;
+            response.reason = e.getMessage();
+        }finally {
+            resp.setContentType("application/json; charset=utf-8");
+            String jsonString = gson.toJson(response);
+            resp.getWriter().write(jsonString);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("application/json; charset=utf-8");
+        OrderServlet.Response response = new OrderServlet.Response();
+
+        try {
+            //      1. 读取 body 中的数据
+            String body = OrderSystemUtil.readBody(req);
+//      2. 把 body 数据解析成 Request 对象（GSON）
+            RegisterServlet.Request request = gson.fromJson(body, RegisterServlet.Request.class);
+
+            UserDao userDao = new UserDao();
+            String name = req.getParameter("name");
+            String numb = request.numb;
+            String address = request.address;
+            int flags = Integer.parseInt(request.flags);
+            String password = request.password;
+//            flags 为 1 是修改密码
+//            flags 为 2 是修改地址
+//            flags 为 3 是修改手机号
+            if (flags == 1) {
+                userDao.setPassword(name, password);
+            } else if (flags == 2) {
+                userDao.setAddress(name, address);
+            } else if (flags == 3) {
+                userDao.setNumb(name, numb);
+            } else {
+                throw new OrderSystemException("未定义的标识符");
+            }
+
+            response.ok = 1;
+            response.reason = "";
+        } catch (OrderSystemException e) {
+            response.ok = 0;
+            response.reason = e.getMessage();
+        }finally {
+            resp.setContentType("application/json; charset=utf-8");
+            String jsonString = gson.toJson(response);
+            resp.getWriter().write(jsonString);
+        }
+    }
 }
